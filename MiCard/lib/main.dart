e7bf7af20958f589019d4c023f9bfc2f3d488433
Pial, Hamidur Rahaman <pial0007l@gmail.com>
2021-02-15 15:51:32 +1030

    Starting mi-card

--- MiCard/lib/main.dart
+++ MiCard/lib/main.dart
@@ -12,42 +12,7 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Center(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Container(
-                  height: 650,
-                  width: 100,
-                  color: Colors.red,
-                ),
-                Container(
-                  height: 650,
-                  width: 100,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Container(
-                        height: 100,
-                        width: 100,
-                        color: Colors.yellow,
-                      ),
-                      Container(
-                        height: 100,
-                        width: 100,
-                        color: Colors.green,
-                      ),
-                    ],
-                  ),
-                ),
-                Container(
-                  height: 650,
-                  width: 100,
-                  color: Colors.blue,
-                ),
-              ],
-            ),
-          ),
+          child: Column(),
         ),
       ),
     );

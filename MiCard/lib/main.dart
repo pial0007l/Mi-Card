b57385bb71d9929a2abcabec943363e30423b995
Pial, Hamidur Rahaman <pial0007l@gmail.com>
2021-02-15 15:46:09 +1030

    Challenge 2 done.

--- MiCard/lib/main.dart
+++ MiCard/lib/main.dart
@@ -8,6 +8,48 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Center(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  height: 650,
+                  width: 100,
+                  color: Colors.red,
+                ),
+                Container(
+                  height: 650,
+                  width: 100,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        height: 100,
+                        width: 100,
+                        color: Colors.yellow,
+                      ),
+                      Container(
+                        height: 100,
+                        width: 100,
+                        color: Colors.green,
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  height: 650,
+                  width: 100,
+                  color: Colors.blue,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
